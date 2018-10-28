54152e89c8a8aabc551052f2c649a54cc2a353ef
DeCode2018 <decode2018@gmail.com>
2018-10-28 15:45:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const driver = {};
+
+function
+updateDriverWithKeyAndValue(driver, key, value){
+  //returns an driver with the original key value pairs and the new key value pair
+  const newDriver = {...driver};
+  newDriver[key] = value;
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key]=value;
+
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key){
+  const newerDriver = driver;
+
+  return delete newerDriver.key;
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+   delete driver[key];
+   return driver;
+
+}
