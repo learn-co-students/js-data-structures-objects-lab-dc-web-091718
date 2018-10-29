626c99081eb1bdb5fa70e840cf642ffa5627dc95
suttonau <sutton_mjbc@icloud.com>
2018-10-28 22:09:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let driver = {};
+ function copyObject(object) {
+	return JSON.parse(JSON.stringify(object));
+}
+ function updateDriverWithKeyAndValue (object, key, value) {
+	let newDriver = copyObject(object);
+	newDriver[key] = value;
+	return newDriver;
+}
+ function destructivelyUpdateDriverWithKeyAndValue(object, key, value){
+	driver[key] = value;
+	return driver;
+}
+ function deleteFromDriverByKey(object, key) {
+	let newDriver = copyObject(object);
+	delete newDriver[key];
+	return newDriver;
+}
+ function destructivelyDeleteFromDriverByKey(object, key) {
+	delete object[key];
+	return object;
+} 
